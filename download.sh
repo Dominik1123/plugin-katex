2b954d511496d08001fc4056888689290c1befee
Dominik1123 <>
2021-02-03 17:42:27 +0100

    Make sure to update katex.min.css and add fonts to git

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ version="0.12.0"
 
 git rm static/fonts/* && mkdir static/fonts
 
-wget -q -P static "https://cdn.jsdelivr.net/npm/katex@$version/dist/katex.min.css"
+wget -q -O static/katex.min.css "https://cdn.jsdelivr.net/npm/katex@$version/dist/katex.min.css"
 
 for url in $(wget -q -O - "https://cdn.jsdelivr.net/npm/katex@$version/dist/fonts/" | grep -ioe "npm/katex@$version/dist/fonts/[a-z0-9_-]\+[.][a-z0-9]\+")
 do
@@ -12,3 +12,5 @@ do
 	wget -q -P static/fonts "https://cdn.jsdelivr.net/$url"
 	echo "done"
 done
+
+git add static/fonts
